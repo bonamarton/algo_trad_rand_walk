e97bf078ea775cacd5d5711e2e810cc78e08c39b
bonamarton <36725693+bonamarton@users.noreply.github.com>
2018-03-15 20:09:28 +0100

    init_GMB
    help.txt modif, add basic geo_brown_motion.cpp

---
+++ stoch_proc/geo_brown_motion.cpp
@@ -0,0 +1,22 @@
+/*
+
+-- c++11 --
+
+ Geometric Brownian Motion
+
+
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+    using namespace std;
+
+
+
+    return 0;
+}
