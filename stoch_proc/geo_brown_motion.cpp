506fa7f7d1f6462581539df73849b68070e1c9b6
bonamarton <36725693+bonamarton@users.noreply.github.com>
2018-03-15 21:20:23 +0100

    GBM_rand_gen

--- stoch_proc/geo_brown_motion.cpp
+++ stoch_proc/geo_brown_motion.cpp
@@ -11,12 +11,21 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <random>
 
 int main()
 {
     using namespace std;
 
+    double dt = 1;
 
+    random_device rd;
+    mt19937 gen(rd());
+    normal_distribution<double> norm_dist(0,sqrt(dt));
+
+    for(int i=0; i<10; i++){
+        cout << norm_dist(gen) << "\n";
+    }
 
     return 0;
 }
